3533bf646ed09732d9ad43fe47cc377257a0709f
Bruno Gama Catão <brunogamacatao@gmail.com>
2012-05-24 12:43:54 -0300

    Upgraded the plugin version

--- ThumbnailGrailsPlugin.groovy
+++ ThumbnailGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class ThumbnailGrailsPlugin {
     // the plugin version
-    def version = "1.2"
+    def version = "1.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
@@ -10,7 +10,6 @@ class ThumbnailGrailsPlugin {
         "grails-app/views/error.gsp"
     ]
 
-    // TODO Fill in these fields
     def title = "Thumbnail Plugin" // Headline display name of the plugin
     def author = "Bruno Gama Catao"
     def authorEmail = ""
@@ -26,48 +25,15 @@ Simple plugin for handling image thumbnails.
     // License: one of 'APACHE', 'GPL2', 'GPL3'
    def license = "APACHE"
 
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+   def issueManagement = [ system: "github", url: "https://github.com/brunogamacatao/thumbnail/issues" ]
 
     // Online location of the plugin's browseable source code.
    def scm = [ url: "https://github.com/brunogamacatao/thumbnail" ]
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
 
     def doWithSpring = {
       thumbnailEditorRegistrar(ThumbnailEditorRegistrar) {
         thumbnailService = ref("thumbnailService")
       }
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
     }
 }
